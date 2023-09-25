7dc01f334ae9602e2a859cc340cf03f837a6fb17
Lukas Lindgren <lukas.lindgren@geoguessr.com>
2023-09-25 20:41:00 +0200

    optimizations

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,20 +4,6 @@ import {
   PlayerWithOccurrencesAndScores,
 } from "../types/types.d";
 
-export const sortPlayersByScore = (array: Player[]) => {
-  return array.sort((a, b) => a.score - b.score);
-};
-
-export const sortTourneysByDate = (array: TourneyData[]) => {
-  return array.sort(
-    (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
-  );
-};
-
-export const filterTourneysByYear = (array: TourneyData[], year: string) => {
-  return array.filter(tourney => tourney.date.substring(0, 4) === year)
-};
-
 const getAllPlayersAndScores = (array: TourneyData[]) => {
   let allPlayersAllScores: any = [];
 
@@ -33,20 +19,30 @@ const getAllPlayersAndScores = (array: TourneyData[]) => {
 const removeHighestScores = (array: PlayerWithOccurrencesAndScores[]) => {
   array.forEach((player: PlayerWithOccurrencesAndScores) => {
     if (player.scores.length === 5) {
-      const max = Math.max(...player.scores);
-      player.scores.splice(player.scores.indexOf(max), 1);
+      player.scores.sort((a, b) => b - a).splice(0, 1);
     }
     if (player.scores.length === 6) {
-      const max = Math.max(...player.scores);
-      player.scores.splice(player.scores.indexOf(max), 1);
-      const secondMax = Math.max(...player.scores);
-      player.scores.splice(player.scores.indexOf(secondMax), 1);
+      player.scores.sort((a, b) => b - a).splice(0, 2);
     }
     const scoreSum = player.scores.reduce((a, b) => a + b, 0);
     player.scores = [scoreSum];
   });
 
   return array;
+};
+
+export const sortPlayersByScore = (array: Player[]) => {
+  return array.sort((a, b) => a.score - b.score);
+};
+
+export const sortTourneysByDate = (array: TourneyData[]) => {
+  return array.sort(
+    (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+  );
+};
+
+export const filterTourneysByYear = (array: TourneyData[], year: string) => {
+  return array.filter((tourney) => tourney.date.substring(0, 4) === year);
 };
 
 export const sortLowOccurrenceLast = (
